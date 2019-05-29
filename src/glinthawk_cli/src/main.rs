4c0ffb0ae94cb882df1195b274836bd6a18f7f2c
sourcepirate <sathyanarrayanan@yandex.com>
2019-05-29 23:58:38 +0800

    updated src main.rs

--- src/glinthawk_cli/src/main.rs
+++ src/glinthawk_cli/src/main.rs
@@ -23,7 +23,7 @@ use rusoto_core::Region;
 use serde::Deserialize;
 
 pub const NAMESPACE: &'static str = "Glinthawk";
-const BUF_SIZE: usize = 60;
+const BUF_SIZE: usize = 15;
 const USAGE: &'static str = "
 Glinthawk Usage
 
